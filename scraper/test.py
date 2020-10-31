3d3ecc6f48aca0ce6d3e05801a0c54ea47706713
AlbertUlysses <albertulysseschavez@gmail.com>
2020-10-31 16:33:09 -0700

    test:add test to check that file exists

--- scraper/test.py
+++ scraper/test.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """Test for scrapescript.py"""
+
+import os
+from subprocess import getstatusoutput, getoutput
+
+prg = './scrapescript.py'
+
+
+# -------------------------------------------------
+def test_exists():
+    """exists"""
+
+    assert os.path.isfile(prg)
+
+
+# -------------------------------------------------
